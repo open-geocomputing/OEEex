5cbf3b5af747d908dbe98b14a47836f9b95cef85
Mathieu Gravey <mathieu@mgravey.com>
2022-01-04 11:21:03 +0100

    Update insertInCE.js

--- insertInCE.js
+++ insertInCE.js
@@ -1,39 +1,57 @@
 function addInsertCEButton(obj,title,lib,editor,isConstructor){
 	let span=document.createElement('span');
 	span.classList.add('insertInCEButton','material-icons')
-	span.innerHTML="keyboard_tab"
-	span.addEventListener('click',function(event){
-		event.stopPropagation();
-		let text=title+'({\n'
-		for (var i = (isConstructor?0: 1); i < lib.arguments.length; i++) {
-			let name=('optional'in lib.arguments[i] && lib.arguments[i].optional? '//':'	')
-			+'	'+lib.arguments[i].argumentName+':'
-			if('defaultValue'in lib.arguments[i]){
-				if(lib.arguments[i].defaultValue==null)
-					name+="null";
-				else if(lib.arguments[i].defaultValue==true)
-					name+="true";
-				else if(lib.arguments[i].defaultValue==false)
-					name+="false";
-				else if(typeof lib.arguments[i].defaultValue ==='string'){
-					name+='"'+lib.arguments[i].defaultValue+'"';
-				}else{
-					name+=lib.arguments[i].defaultValue;
+	span.innerHTML="keyboard_tab";
+	if('arguments' in lib)
+		span.addEventListener('click',function(event){
+			event.stopPropagation();
+			let text=title+'({\n'
+			for (var i = (isConstructor ? 0 : 1); i < lib.arguments.length; i++) {
+				let name=('optional'in lib.arguments[i] && lib.arguments[i].optional? '//':'	')
+				+'	'+lib.arguments[i].argumentName+':'
+				if('defaultValue'in lib.arguments[i]){
+					if(lib.arguments[i].defaultValue==null)
+						name+="null";
+					else if(lib.arguments[i].defaultValue==true)
+						name+="true";
+					else if(lib.arguments[i].defaultValue==false)
+						name+="false";
+					else if(typeof lib.arguments[i].defaultValue ==='string'){
+						name+='"'+lib.arguments[i].defaultValue+'"';
+					}else{
+						name+=lib.arguments[i].defaultValue;
+					}
 				}
+				name+=',\n';
+				text+=name;
 			}
-			name+=',\n';
-			text+=name;
-		}
-		text+='})'
-		if(lib.arguments.length<2){
-			text=title+'()';
-		}
-		editor.insert(text)
-	})
+			text+='})'
+			if(lib.arguments.length<(1+!isConstructor)){
+				text=title+'()';
+			}
+			editor.insert(text)
+		})
+	else
+		span.addEventListener('click',function(event){
+			event.stopPropagation();
+			let text=title+'({\n'
+			for (var i = (isConstructor ? 0 : 1); i < lib.args.length; i++) {
+				let name=('optional'in lib.args[i] && lib.args[i].optional? '//':'	')
+				+'	'+lib.args[i].name+':'
+				name+=',\n';
+				text+=name;
+			}
+			text+='})'
+			if(lib.args.length<(1+!isConstructor)){
+				text=title+'()';
+			}
+			editor.insert(text)
+		})
 	obj.appendChild(span);
+	obj.classList.add('OEEenhanced');
 }
 
-function getAllDocObject(libSignatures){
+function getAllDocObject(allSignatures){
 
 	let editorElement=document.getElementsByClassName('ace_editor')
 	if(editorElement && editorElement.length>0){
@@ -41,57 +59,173 @@ function getAllDocObject(libSignatures){
 		var editor = ace.edit("editor");
 	}
 
-
-	let allSignatures={}
-	for (var i = libSignatures['algorithms'].length - 1; i >= 0; i--) {
-		allSignatures[libSignatures['algorithms'][i].name]=libSignatures['algorithms'][i];
-	}
 	let docList=document.querySelector('.docs-list .main-section')
-	docList.querySelectorAll('.docs-method-header.constructor').forEach(e=>{
+	docList.querySelectorAll('.docs-method-header.constructor:not(.OEEenhanced)').forEach(e=>{
 		let signature='algorithms/'+(e.innerText.split('(')[0]).slice(3);
 		if(signature in allSignatures)
-			addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,true);
-	})
-	docList.querySelectorAll('.docs-method-header:not(.constructor)').forEach(e=>{
-		let signature='algorithms/'+(e.parentNode.parentNode.firstChild.innerText+'.'+e.innerText.split('(')[0]).slice(3);
-		if(signature in allSignatures)
-			addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
-		else
 		{
-			signature='algorithms/'+(e.innerText.split('(')[0]).slice(3);
-			if(signature in allSignatures)
-				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
-
+			addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,true);
+			return;
+		}
+		{
+			signature=(e.parentNode.parentNode.firstChild.innerText+'.prototype.'+e.innerText.split('(')[0])
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,true);
+				return;
+			}
+			signature="playground.api."+signature;
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,true);
+				return;
+			}
 		}
 
+		{
+			signature=e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,true);
+				return;
+			}
+			signature="playground.api."+signature;
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,true);
+				return;
+			}
+		}
+	})
+	docList.querySelectorAll('.docs-method-header:not(.constructor):not(.OEEenhanced)').forEach(e=>{
+		let signature='algorithms/'+(e.parentNode.parentNode.firstChild.innerText+'.'+e.innerText.split('(')[0]).slice(3);
+		if(signature in allSignatures)
+		{
+			addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+			return;
+		}
+		else
+		{
+			signature=e.innerText.split('(')[0];
+			if(signature.startsWith('ee.Algorithms.'))
+				signature=signature.slice('ee.Algorithms.'.length);
+			if(signature.startsWith('ee.'))
+				signature=signature.slice(3);
+			signature='algorithms/'+signature;
+			if(signature in allSignatures)
+			{
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		{
+			signature='algorithms/Geometry.'+e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		{
+			signature='algorithms/reduce.'+e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		{
+			signature='algorithms/Collection.'+e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+
+		{
+			signature='algorithms/Element.'+e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		{
+			signature='algorithms/Window.'+e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		if(e.innerText.split('(')[0].startsWith('aggregate_')){
+			signature='algorithms/AggregateFeatureCollection.'+e.innerText.split('(')[0].slice('aggregate_'.length);
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		{
+			signature=(e.parentNode.parentNode.firstChild.innerText+'.prototype.'+e.innerText.split('(')[0])
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+			signature="playground.api."+signature;
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
+
+		{
+			signature=e.innerText.split('(')[0];
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+			signature="playground.api."+signature;
+			if(signature in allSignatures){
+				addInsertCEButton(e,e.innerText.split('(')[0],allSignatures[signature],editor,false);
+				return;
+			}
+		}
 	})
 }
 
 
 function loadFunctionSignatures(observer){
 	this.disconnect();
+	
+	let loadSignature2=new XMLHttpRequest();
+	loadSignature2.open("GET",'https://code.earthengine.google.com/docs/get',true);
+	loadSignature2.responseType = 'json';
+	loadSignature2.onload = function(e) {
+		if (this.status == 200) {
+			getAllDocObject(this.response);
+		}
+		
+	}
+	//loadSignature.setRequestHeader("Authorization", ee.data.getAuthToken());
+	
+
 	let loadSignature=new XMLHttpRequest();
 	loadSignature.open("GET",'https://content-earthengine.googleapis.com/v1alpha/projects/earthengine-legacy/algorithms?prettyPrint=false',true);
 	loadSignature.responseType = 'json';
 	loadSignature.onload = function(e) {
 		if (this.status == 200) {
-			getAllDocObject(this.response);
+			let libSignatures=this.response;
+			let allSignatures={}
+			for (var i = libSignatures['algorithms'].length - 1; i >= 0; i--) {
+				allSignatures[libSignatures['algorithms'][i].name]=libSignatures['algorithms'][i];
+			}
+			loadSignature2.send();
+			getAllDocObject(allSignatures);
 		}
 	}
 	loadSignature.setRequestHeader("Authorization", ee.data.getAuthToken());
 	loadSignature.send();
 
-	// let loadSignature=new XMLHttpRequest();
-	// loadSignature.open("GET",'https://code.earthengine.google.com/docs/get',true);
-	// loadSignature.responseType = 'json';
-	// loadSignature.onload = function(e) {
-	// 	if (this.status == 200) {
-	// 		getAllDocObject(this.response);
-	// 	}
-		
-	// }
-	// //loadSignature.setRequestHeader("Authorization", ee.data.getAuthToken());
-	// loadSignature.send();
+	
 }
 
 (function(){
