567d533a1537a1db566bfc351b7f5734333976d9
Mathieu Gravey <mathieu@mgravey.com>
2022-11-02 19:22:25 +0100

    add dbclick on script

--- injectionScript.js
+++ injectionScript.js
@@ -1,4 +1,5 @@
-listOfScript=['isShareable','insertInCE','planetLab','uploadWithManifest','hackEE','EEDarkMode','addCommandS','runAllTasks','addPlotly','3rd_party/shpwrite','addCopyJSON'];
+listOfScript=['isShareable','insertInCE','planetLab','uploadWithManifest','hackEE','EEDarkMode',
+				'addCommandS','runAllTasks','addPlotly','3rd_party/shpwrite','addCopyJSON','openScriptNewTab'];
 chrome.storage.local.get(listOfScript, function(result){
 	result['3rd_party/shpwrite']=result['uploadWithManifest'];
 	for (var i = listOfScript.length - 1; i >= 0; i--) {
---
+++ openScriptNewTab.js
@@ -0,0 +1,65 @@
+var oee_dbclickScriptTimeout=null;
+
+
+function addScriptPath(targetNode){
+// document.querySelector(".tree-manager.repo-manager").querySelectorAll('.file-type-file .tree-item-name')[0].closest('ee-zippy').parentElement.closest('ee-zippy, .zippy')
+// 	[...targetNode.querySelectorAll('zippy')]
+// .map(e => console.log(e))
+
+	function constructPath(e,start){
+		let parent=e.parentElement.closest('ee-zippy, .zippy')
+		if(parent.classList.contains('zippy')){
+			// the end of the recursion
+			let name=parent.querySelector('.zippy > .header .tree-item-name').innerText;
+			return name+':'+start;
+		}else{
+			// get header
+			let name=parent.querySelector('ee-zippy > .header .tree-item-name').innerText;
+			//and ast the other part
+			return constructPath(parent, name+'/'+start)
+		}
+	}
+
+	let nodesToExplore=targetNode.map(e=>e.addedNodes[0]).filter(e=> e.classList.contains('file-type-file'));
+
+	nodesToExplore.map(e=>{
+		let path=constructPath(e,e.querySelector('.tree-item-name').innerText)
+		e.addEventListener('click',event=>{
+			if(!event.detail?.timeoutClick){
+				event.preventDefault();
+				event.stopPropagation();
+
+				if(oee_dbclickScriptTimeout){
+					clearTimeout(oee_dbclickScriptTimeout);
+					oee_dbclickScriptTimeout=null;
+					window.open("https://code.earthengine.google.com/?scriptPath="+encodeURIComponent(path), '_blank')
+				}else{
+					var event = new CustomEvent("click", {'detail':{'timeoutClick': true}});
+					oee_dbclickScriptTimeout=setTimeout(function(){e.dispatchEvent(event)},300)
+				}
+			}else{
+				oee_dbclickScriptTimeout=null;
+			}
+
+			
+		},true);
+	})
+}
+
+function addObeserver(){
+	let targetNode=document.querySelector(".tree-manager.repo-manager")
+	const config = { childList: true, subtree: true };
+
+	// Callback function to execute when mutations are observed
+	const callback = (mutationList, observer) => {
+		addScriptPath(mutationList)
+	};
+
+	// Create an observer instance linked to the callback function
+	const observer = new MutationObserver(callback);
+
+	// Start observing the target node for configured mutations
+	observer.observe(targetNode, config);
+}
+
+addObeserver();
