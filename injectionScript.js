13c20a47c8ef2582b8f5cc9c7636340e21a53e1f
Mathieu Gravey <mathieu@mgravey.com>
2023-01-20 18:16:07 +0100

    fix last incompatibilities with firefox

--- injectionScript.js
+++ injectionScript.js
@@ -1,31 +1,72 @@
-listOfScript=['isShareable',
-	'insertInCE',
-	'planetLab',
-	'uploadWithManifest',
-	'hackEE',
-	'EEDarkMode',
-	'addCommandS',
-	'runAllTasks',
-	'addPlotly',
-	'3rd_party/shpwrite',
-	'addCopyJSON',
-	'editorSettings',
-	'surveyMessage',
-	'openScriptNewTab'
-];
-chrome.storage.local.get(listOfScript,
-function(result){
-	result['3rd_party/shpwrite']=result['uploadWithManifest'];
-	for (var i = listOfScript.length - 1; i >= 0; i--) {
-		let key=listOfScript[i]
-		if(result[key]){
-			var s = document.createElement('script');
-			s.src = chrome.runtime.getURL(key+'.js');
-			s.onload = function() {
-				this.remove();
-			};
-			(document.head || document.documentElement).appendChild(s);
+function injectScripts(){
+	let listOfScript=['isShareable',
+		'insertInCE',
+		'planetLab',
+		'uploadWithManifest',
+		'hackEE',
+		'EEDarkMode',
+		'addCommandS',
+		'runAllTasks',
+		'addPlotly',
+		'3rd_party/shpwrite',
+		'addCopyJSON',
+		'editorSettings',
+		'surveyMessage',
+		'openScriptNewTab',
+	];
+
+	chrome.storage.local.get(listOfScript,
+		function(result){			
+			result['3rd_party/shpwrite']=result['uploadWithManifest'];
+			for (var i = listOfScript.length - 1; i >= 0; i--) {
+				let key=listOfScript[i]
+				if(result[key]){
+					var s = document.createElement('script');
+					s.src = chrome.runtime.getURL(key+'.js');
+					s.onload = function() {
+						this.remove();
+					};
+					(document.head || document.documentElement).appendChild(s);
+				}
+			}
+		});
+}
+
+isFirefox=typeof(trustedTypes)=="undefined";
+
+if(isFirefox){
+	let listConnector={};
+	window.addEventListener("message", (event) => {
+		if (event.source == window &&
+			event.data &&
+			event.data.direction == "p2e") {
+			if(listConnector[event.data.connectInfo.name]==undefined){
+				listConnector[event.data.connectInfo.name]=browser.runtime.connect(browser.runtime.id,event.data.connectInfo);
+				listConnector[event.data.connectInfo.name].onMessage.addListener(function(val){
+					window.postMessage({message:val,connectInfo:event.data.connectInfo,direction:"e2p"})
+				})
+				listConnector[event.data.connectInfo.name].onDisconnect.addListener((p) => {
+					delete listConnector[event.data.connectInfo.name];
+				});
+			}
+			listConnector[event.data.connectInfo.name].postMessage(event.data.message);
 		}
-	}
-});
+	});
+
+	var s = document.createElement('script');
+	s.src = chrome.runtime.getURL('3rd_party/trustedtypes.api_only.build.js');
+	s.onload = function() {
+		this.remove();
+		var s = document.createElement('script');
+		s.src = chrome.runtime.getURL('externalConenctingfix.js');
+		s.onload = function() {
+			this.remove();
+			injectScripts();
+		};
+		(document.head || document.documentElement).appendChild(s);
+	};
+	(document.head || document.documentElement).appendChild(s);
+}else{
+	injectScripts();
+}
 
--- injectionScriptApp.js
+++ injectionScriptApp.js
@@ -1,16 +1,33 @@
-listOfScript=['addPlotly'];
 
-chrome.storage.local.get(listOfScript, function(result){
-	for (var i = listOfScript.length - 1; i >= 0; i--) {
-		let key=listOfScript[i]
-		if(result[key]){
-			var s = document.createElement('script');
-			s.src = chrome.runtime.getURL(key+'.js');
-			s.onload = function() {
-				this.remove();
-			};
-			(document.head || document.documentElement).appendChild(s);
+
+function injectScripts(){
+	let listOfScript=['addPlotly'];
+
+	chrome.storage.local.get(listOfScript, function(result){
+		for (var i = listOfScript.length - 1; i >= 0; i--) {
+			let key=listOfScript[i]
+			if(result[key]){
+				var s = document.createElement('script');
+				s.src = chrome.runtime.getURL(key+'.js');
+				s.onload = function() {
+					this.remove();
+				};
+				(document.head || document.documentElement).appendChild(s);
+			}
 		}
-	}
-});
+	});
+}
 
+isFirefox=typeof(trustedTypes)=="undefined";
+
+if(isFirefox){
+	var s = document.createElement('script');
+	s.src = chrome.runtime.getURL('3rd_party/trustedtypes.api_only.build.js');
+	s.onload = function() {
+		this.remove();
+		injectScripts();
+	};
+	(document.head || document.documentElement).appendChild(s);
+}else{
+	injectScripts();
+}
