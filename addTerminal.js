869b2cbcfe8b7f7c5d1247f34196a199736735c3
Mathieu Gravey <mathieu@mgravey.com>
2023-02-14 20:09:34 +0100

    add F5

--- addTerminal.js
+++ addTerminal.js
@@ -10,6 +10,8 @@ if(typeof OEEexEscape == 'undefined'){
 	});
 }
 
+function sendCodeFromEditor2Terminal(){};
+
 function loadConsoleTerminalWatcher(){
 	let MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
 	let myObserver          = new MutationObserver(function(mutList){
@@ -24,6 +26,12 @@ function loadConsoleTerminalWatcher(){
 		});
 	});
 	let obsConfig = { childList: true};
+
+	document.addEventListener('keydown',function(event) {
+		if(/*(event.metaKey) &&*/ event.which == 116) {
+			sendCodeFromEditor2Terminal();
+		}
+	});
 	
 	if(document.querySelector('ee-console'))
 		myObserver.observe(document.querySelector('ee-console'), obsConfig);
@@ -40,6 +48,48 @@ function analysisTerminalAddon(val){
 			s.textContent=">>";
 			obj.parentNode.insertBefore(s,obj);
 		}
+
+		// add F5 shortcut
+		let editorElement=document.getElementsByClassName('ace_editor');
+		if(editorElement && editorElement.length>0){
+			editorElement[0].id='editor'
+			editor = ace.edit("editor");
+		}
+		let current="";
+		let historyCmd=[""];
+		let postionHitory=0;
+
+		input.addEventListener('change', (event) => {
+			historyCmd[0]=event.target.value;
+			if(historyCmd[0]!="") historyCmd.unshift("");
+		},true);
+
+		sendCodeFromEditor2Terminal=function(){
+			input.value=editor.getSelectedText().replaceAll("\n","\\n");
+			input.dispatchEvent(new Event('change'));
+		}
+
+		input.addEventListener('keydown',function(event) {
+			let skip=false;
+			if(event.which == 13){
+				input.dispatchEvent(new Event('change'));
+			}
+			if( event.which == 38 && historyCmd.length>0) {//up
+				postionHitory=(++postionHitory>=historyCmd.length?historyCmd.length-1:postionHitory);
+				skip=true;
+			}
+			if( event.which == 40 && historyCmd.length>0) {// down
+				postionHitory=(--postionHitory<0?0:postionHitory);
+				skip=true;
+			}
+			if(skip){
+				input.value=historyCmd[postionHitory];
+				input.selectionEnd = historyCmd[postionHitory].length;
+				input.selectionStart = historyCmd[postionHitory].length;
+			}else{
+				historyCmd[0]=input.value;
+			}
+		});
 	});
 }
 
