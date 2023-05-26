b419fba2a8c315729dfff5c8e4b92fac315dfb2f
Mathieu Gravey <mathieu@mgravey.com>
2023-05-26 11:41:21 +0200

    Update pythonCE.js

--- pythonCE.js
+++ pythonCE.js
@@ -27,6 +27,8 @@ function injectPythonCE(){
 			`).then(function(){
 				pyee=pyodide.pyimport("ee");
 				console.log("ee charge in Py env")
+				pyee.Initialize()
+				console.log("ee is initialized")
 			})
 		})
 
@@ -51,3 +53,56 @@ function requestAsEE(uri){
 
 injectPythonCE();
 
+function loadConsolePythonCEWatcher(){
+	let MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
+	let myObserver          = new MutationObserver(function(mutList){
+
+		[...mutList].map(function(mut){
+			[...mut.addedNodes].map(function(e){
+				if(e.classList.contains('OEEexPythonCEAnalysis'))
+					return;
+				e.classList.add('OEEexPythonCEAnalysis')
+				analysisPythonCEAddon(e)
+			});
+		});
+	});
+	let obsConfig = { childList: true};
+	
+	if(document.querySelector('ee-console'))
+		myObserver.observe(document.querySelector('ee-console'), obsConfig);
+}
+
+function analysisPythonCEAddon(val){
+	val.querySelectorAll('.ui-widget.ui-textbox').forEach(function(obj){
+		let input=obj.querySelector('input');
+		if(input.placeholder=="OEEex_Active_AddonPythonCE"){
+			input.style.display='none';
+			runSendPython(input);
+		}
+	});
+}
+
+function runSendPython(inputElement){
+	let jsInput=JSON.parse(inputElement.value);
+	switch
+	if(jsInput.type=="code"){
+		
+	}
+	switch (jsInput.type) {
+	case 'code':
+		pyodide.runPythonAsync(jsInput.code)
+		break;
+	case 'fucntionCall':
+		if(jsInput.inputs)
+		""
+		"oeelValReturnValue=${jsInput.name}("++");"
+		
+		console.log('Mangoes and papayas are $2.79 a pound.');
+		break;
+	default:
+		console.log(`Sorry ${jsInput.type} is unsuported.`);
+	}
+
+}
+
+loadConsolePythonCEWatcher();
