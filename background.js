046108c589ebf9e2efa7a2e1f0bc2f1d2152c527
Mathieu Gravey <mathieu@mgravey.com>
2023-06-18 23:13:11 +0200

    move addCommandS to standard editor settings

--- background.js
+++ background.js
@@ -9,19 +9,21 @@ chrome.runtime.onInstalled.addListener(function() {
 		uploadWithManifest:true,
 		hackEE:false, //remove in the future
 		EEDarkMode:true,
-		addCommandS:navigator.platform.toLowerCase().includes('mac'),
 		runAllTasks:true,
 		oeelCache:true,
 		addCopyJSON:true,
 		openScriptNewTab:true,
 		editorSettings:true,
+		EStabSize:2,
 		ESfontSize:13,
 		ESfontFamily:"default",
+		ES_SC:(navigator.platform.toLowerCase().includes('mac')?{Execute: 'Command+Enter', 'Execute With Profiler': 'Alt+Command+Enter', Save: 'Command+S', Search: 'Alt+Command+F', Suggestion: 'Ctrl+Alt+Command+Space', alignCursors: 'Alt+Command+A'}:{}),
 		addTerminal:true,
 		surveyMessage:true,
 		addPlotly:true
 	});
 });
+
 
 chrome.action.onClicked.addListener(tab => {
 	if(tab.url.startsWith('https://code.earthengine.google.com')){
@@ -109,8 +111,8 @@ function sendESConfig(ports=listEditorPort){
 		ports=[ports];
 	}
 
-	chrome.storage.local.get(['ESfontSize','ESfontFamily'], function(data) {
-		ports.map((sender)=>sender.postMessage({ type:'changeFont', message: data }));
+	chrome.storage.local.get(['ESfontSize','ESfontFamily','EStabSize','ES_SC'], function(data) {
+		ports.map((sender)=>sender.postMessage({ type:'EditorSetting', message: data }));
 	});
 }
 
--- editorSettings.js
+++ editorSettings.js
@@ -1,11 +1,43 @@
 var OEEexidString=document.currentScript.src.match("([a-z]{32})|([0-9a-f-]{36})")[0];
 
+let oeel_ECSet=null;
+function setECSettings(request,root){
+	if(typeof(editor)=="undefined"){
+		let editorElement=document.getElementsByClassName('ace_editor')
+		if(typeof(editor)=="undefined" && editorElement && editorElement.length>0){
+			editorElement[0].id='editor'
+			editor = ace.edit("editor");
+		}
+	}
+	if(typeof(editor)=="undefined") return;
+	clearTimeout(oeel_ECSet);
+	oeel_ECSet=null;
+	if(request.type=='EditorSetting'){
+
+		console.log(request.message)
+		if (request.message.ESfontSize) root.style.setProperty('--editorFontSize', request.message.ESfontSize + "px");
+		if (request.message.ESfontFamily){
+			root.style.setProperty('--editorFontFamily', request.message.ESfontFamily);
+			if (request.message.ESfontFamily=='default')
+				root.style.setProperty('--editorFontFamily', "'Menlo','Monaco','DejaVu Sans Mono','Bitstream Vera Sans Mono','Consolas','source-code-pro',monospace");
+		}	
+		if (request.message.EStabSize) editor.setOption('tabSize',request.message.EStabSize)
+		if (request.message.ES_SC){
+			for (const [key, value] of Object.entries(request.message.ES_SC)) {
+				editor.commands.commands[key].bindKey.mac=value;
+				editor.commands.commands[key].bindKey.win=value;
+			}
+			editor.commands.addCommands(editor.commands.commands)
+		}
+	};
+}
+
 function initEditorSettings(){
 
 	var sheet = new CSSStyleSheet
 	sheet.replaceSync( ":root{--editorFontSize:13px; --editorFontFamily:'Menlo','Monaco','DejaVu Sans Mono','Bitstream Vera Sans Mono','Consolas','source-code-pro',monospace }\n \
-	  #editor.ace_editor.enable-suggestions .stan-underline{ height: calc( var(--editorFontSize, 13px) - 1px) !important; }\
-	  .env-list .zippy .header{font-size:calc( var(--editorFontSize, 13px) + 1px)}")
+		#editor.ace_editor.enable-suggestions .stan-underline{ height: calc( var(--editorFontSize, 13px) - 1px) !important; }\
+		.env-list .zippy .header{font-size:calc( var(--editorFontSize, 13px) + 1px)}")
 	// Append your style to the existing style sheet.
 	document.adoptedStyleSheets=[...document.adoptedStyleSheets,sheet];
 
@@ -27,16 +59,11 @@ function initEditorSettings(){
 	let portWithBackground=null;
 
 	function setPortWithBackground(){
+		console.log("setPortWithBackground")
 		portWithBackground= chrome.runtime.connect(OEEexidString,{name: "oeel.extension.editorSettings"});
 		portWithBackground.onMessage.addListener((request, sender, sendResponse) => {
-			if(request.type=='changeFont'){
-				if (request.message.ESfontSize)		root.style.setProperty('--editorFontSize', request.message.ESfontSize + "px");
-				if (request.message.ESfontFamily){
-					root.style.setProperty('--editorFontFamily', request.message.ESfontFamily);
-					if (request.message.ESfontFamily=='default')
-						root.style.setProperty('--editorFontFamily', "'Menlo','Monaco','DejaVu Sans Mono','Bitstream Vera Sans Mono','Consolas','source-code-pro',monospace");
-				}	
-			};
+			if(oeel_ECSet)clearTimeout(oeel_ECSet);
+			oeel_ECSet=setInterval(setECSettings,10,request,root);
 		})
 		portWithBackground.onDisconnect.addListener(function(port){	
 			portWithBackground=null;
@@ -46,5 +73,4 @@ function initEditorSettings(){
 
 	setPortWithBackground();
 }
-
 initEditorSettings();
